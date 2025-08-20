0448a4cb3bc1d4d8c8a4e2d1125987ecd46c08a6
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2025-08-21 00:08:43 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int m = 45;
+    double t = 45;
+    float e = 45;
+    printf ("m: %d\n", m);
+    printf ("t: %f\n", t);
+    printf ("e: %d\n", e);
+    return 0;
+}
