aedba2550478290bd3aac0d76791b55d54edcfb3
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2025-10-16 18:31:04 +0530

    Add recursive Fibonacci function and user input
    Implemented a recursive function to calculate Fibonacci numbers and added user input for series generation.

--- Fibonacci/By recursion.c
+++ Fibonacci/By recursion.c
@@ -1,1 +1,30 @@
+# include <stdio.h>
 
+int fibonacci (int n){
+    if (n<=0){
+        return 0;
+    }
+    else if (n==1){
+        return 1;
+    }
+    else{
+        return fibonacci (n-1) + fibonacci (n-2);
+    }
+}
+
+int main (){
+    int c, r;
+    printf ("\nThis code finds the fibonacci number for you.\n");
+    printf ("Enter the number: ");
+    scanf ("%d", &c);
+
+    printf ("The series are: \n");
+    for (int i =0 ; i<c; i++){
+        printf ("%d\t", fibonacci (i));
+    }
+    printf ("\n");
+
+    
+    return 0;
+
+}
